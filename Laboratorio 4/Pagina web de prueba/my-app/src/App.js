49cc759a48517580bc4aabfeaf3f2d39b56e39dd
Omar Marca <122169269+xeromark@users.noreply.github.com>
2024-06-14 20:46:11 -0400

    Add files via upload

---
+++ Laboratorio 4/Pagina web de prueba/my-app/src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Con cripto se pueden hacer muchas cosas. Regularmente funcionan. Imaginen lo que se puede hacer con cripto. Por lo tanto hay que escogerlo. Tenemos que aprender de cripto. Opinemos sobre crypto. Con cripto se pueden hacer muchas cosas. Regularmente funcionan. Imaginen lo que se puede hacer con cripto. Por lo tanto hay que escogerlo. Tenemos que aprender de cripto. Opinemos sobre crypto. Con cripto se pueden hacer muchas cosas. Regularmente funcionan. Imaginen lo que se puede hacer con cripto. Por lo tanto hay que escogerlo. Tenemos que aprender de cripto. Opinemos sobre crypto. Con cripto se pueden hacer muchas cosas. Regularmente funcionan. Imaginen lo que se puede hacer con cripto. Por lo tanto hay que escogerlo. Tenemos que aprender de cripto. Opinemos sobre crypto. 
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <div class="M1" id="yBaaSwxjXWOBwefIbNBz/g=="> </div>
+      <div class="M2" id="4PRw0NDwUiE="> </div>
+      <div class="M3" id="/cKZsKBNsrbBY/mx4Fu/Sw=="> </div>
+      <div class="M4" id="Zbp4YwxXFb4="> </div>
+      <div class="M5" id="vXdj5wcK7Y4="> </div>
+
+    </div>
+  );
+}
+
+export default App;
