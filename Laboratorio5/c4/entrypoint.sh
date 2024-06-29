bff311749201d039c6809cb1553fa2f52c710e34
Omar Marca <122169269+xeromark@users.noreply.github.com>
2024-06-28 20:49:03 -0400

    Add files via upload

---
+++ Laboratorio5/c4/entrypoint.sh
@@ -0,0 +1,5 @@
+# Iniciar el servicio SSH
+service ssh start
+
+# Para que el contenedor siga en ejecución
+tail -f /dev/null
---
+++ Laboratorio5/c4/sources.list.sh
@@ -0,0 +1,5 @@
+## EOL upgrade sources.list
+# Required
+deb http://old-releases.ubuntu.com/ubuntu/ CODENAME main restricted universe multiverse
+deb http://old-releases.ubuntu.com/ubuntu/ CODENAME-updates main restricted universe multiverse
+deb http://old-releases.ubuntu.com/ubuntu/ CODENAME-security main restricted universe multiverse
