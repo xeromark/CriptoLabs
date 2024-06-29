d5ba676ae579d824a799f6de778dcd2b76f13343
Omar Marca <122169269+xeromark@users.noreply.github.com>
2024-06-28 20:49:25 -0400

    Delete Laboratorio5 directory

--- Laboratorio5/c2/sources.list.sh
+++
@@ -1,5 +0,0 @@
-## EOL upgrade sources.list
-# Required
-deb http://old-releases.ubuntu.com/ubuntu/ CODENAME main restricted universe multiverse
-deb http://old-releases.ubuntu.com/ubuntu/ CODENAME-updates main restricted universe multiverse
-deb http://old-releases.ubuntu.com/ubuntu/ CODENAME-security main restricted universe multiverse
